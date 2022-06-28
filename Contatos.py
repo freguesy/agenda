8e571ac75f134b00fe5afa51a67e61bb77c02fa7
freguesy <freguesy@gmail.com>
2022-06-28 10:27:41 -0300

    1

--- Contatos.py
+++
@@ -1,5 +0,0 @@
-class Contato:
-    def __init__(self,argumento1,argumento2,argumento3):
-        self.cod = argumento1
-        self.nome = argumento2
-        self.telefone = argumento3
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from Agenda import*
+from func import*
 from classe_menu import*
-
-#agenda1 = Agenda()
-#agenda1.salvar_contatos()
-#agenda1.salvar_contatos()
-#agenda1.listar_contatos()
 
 menu = Menu()
